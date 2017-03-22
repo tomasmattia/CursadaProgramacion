e42da29738b83226c346f0f90bfc6b24cb315f6b
Tomas Mattia <tomasmattia@users.noreply.github.com>
2017-03-22 00:07:11 -0300

    Add files via upload

--- Programacion I/ACUMULADORES. PRESENTACIÓN EN PANTALLA. BUCLES Y TOMA DE DECISIONES/Ejercicio10/main.c
+++ Programacion I/ACUMULADORES. PRESENTACIÓN EN PANTALLA. BUCLES Y TOMA DE DECISIONES/Ejercicio10/main.c
@@ -3,6 +3,25 @@
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    int numero;
+    int sumaPositivos=0;
+    int productoNegativos=1;
+    int contador;
+    for(contador=0;contador<10;contador++){
+        printf("Ingrese un numero: ");
+        scanf("%d",&numero);
+        system("cls");
+        while(numero==0){
+                printf("Ingrese un numero que no sea 0: ");
+                scanf("%d",&numero);
+            }
+        if(numero>0){
+            sumaPositivos+=numero;
+        }
+        else{
+            productoNegativos*=numero;
+        }
+        system("cls");
+    }
+    printf("La suma de todos los positivos da %d y el producto de los negativos %d",sumaPositivos,productoNegativos);
 }
