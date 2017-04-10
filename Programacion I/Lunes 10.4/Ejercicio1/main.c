afc07b38b5e6eb3c49b62217268ce51745576122
Tomas Mattia <tomasmattia@users.noreply.github.com>
2017-04-10 09:31:58 -0300

    Add files via upload

---
+++ Programacion I/Lunes 10.4/Ejercicio1/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char nombre[20];
+    printf("Ingrese su nombre: ");
+    scanf("%s",nombre);
+    printf("Usted se llama %s",nombre);
+    return 0;
+}
