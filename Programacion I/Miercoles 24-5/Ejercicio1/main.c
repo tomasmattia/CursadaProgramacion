34b5d3660c9229b6122a277996c8a400cf90a28a
Tomas Mattia <tomasmattia@users.noreply.github.com>
2017-05-24 12:25:53 -0300

    Add files via upload

--- Programacion I/Miercoles 24-5/Ejercicio1/main.c
+++ Programacion I/Miercoles 24-5/Ejercicio1/main.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// sprintf(VARIABLE DONDE ALMACENA, LO QUE SE ALMACENA);
+/*
+    rewind(bin);
+    while(!feof(bin))
+    {
+        fread();
+        if(codigo==prod.cod)
+        {
+            printf();
+            precio
+            scanf("%f",&prod.cod);
+            fflush(stdin);
+            fseek(ARCHIVO,(-1)*sizeof(ESTRUCTURA),SEEK_CURL);
+            fwrite(&ESTRUCTURA,sizeof(ESTRUCTURA),CANTIDAD A MODIFICAR,ARCHIVO);
+        }
+    }
+
+
+
+*/
+
+
 int main()
 {
     printf("Hello world!\n");
