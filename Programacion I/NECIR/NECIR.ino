f626c7defbb7ed1383781e5b96dfdf064e648653
Tomas Mattia <tomasmattia@users.noreply.github.com>
2017-06-23 11:50:34 -0300

    Add files via upload

---
+++ Programacion I/NECIR/NECIR.ino
@@ -0,0 +1,32 @@
+#include <NECIRrcv.h>
+#include <Arduino.h>
+int pin=2;
+int led=3;
+int flag=0;
+NECIRrcv IR(pin);
+unsigned long valor;
+void setup() {
+  // put your setup code here, to run once:
+  IR.begin();
+  Serial.begin(9600);
+  pinMode(led,OUTPUT);
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+    while(IR.available())
+    {
+      valor=IR.read();
+        switch(valor)
+        {
+          case 3810328320:
+            Serial.println("OK");
+            digitalWrite(led,HIGH);
+            delay(50);
+            digitalWrite(led,LOW);
+            break;
+          default:
+            break;
+        }
+       
+     }
+ }
