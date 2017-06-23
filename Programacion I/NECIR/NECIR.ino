50983982af4565f05f93bd44ce5b95d02d64a9c9
Tomas Mattia <tomasmattia@users.noreply.github.com>
2017-06-23 12:09:57 -0300

    Add files via upload

--- Programacion I/NECIR/NECIR.ino
+++ Programacion I/NECIR/NECIR.ino
@@ -16,20 +16,65 @@ void loop() {
     while(IR.available())
     {
       valor=IR.read();
+      digitalWrite(led,HIGH);
         switch(valor)
         {
           case 3810328320:
             Serial.println("OK");
-            digitalWrite(led,HIGH);
-            flag=1-flag;
-            if(flag==0)
-            {
-              digitalWrite(led,LOW);
-            }
             break;
+          case 3125149440:
+            Serial.println("BOTON 1");
+            break;
+          case 3108437760:
+            Serial.println("BOTON 2");
+            break;
+          case 3091726080:
+            Serial.println("BOTON 3");
+            break;
+          case 3141861120:
+            Serial.println("BOTON 4");
+            break;
+          case 3208707840:
+            Serial.println("BOTON 5");
+            break;
+          case 3158572800:
+            Serial.println("BOTON 6");
+            break;
+          case 4161273600:
+            Serial.println("BOTON 7");
+            break;
+          case 3927310080:
+            Serial.println("BOTON 8");
+            break;
+          case 4127850240:
+            Serial.println("BOTON 9");
+            break;
+          case 3910598400:
+            Serial.println("BOTON *");
+            break;
+          case 3860463360:
+            Serial.println("BOTON 0");
+            break;
+          case 4061003520:
+            Serial.println("BOTON #");
+            break;
+          case 3877175040:
+            Serial.println("FLECHA UP");
+            break;
+          case 4144561920:
+            Serial.println("FLECHA LEFT");
+            break;
+          case 2774204160:
+            Serial.println("FLECHA RIGHT");
+            break;
+          case 2907897600:
+            Serial.println("FLECHA DOWN");
+            break; 
           default:
+            Serial.println(valor);
             break;
         }
-       
+       delay(100);
+       digitalWrite(led,LOW);
      }
  }
